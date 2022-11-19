f017bdef9891a7672e83c65de63d0040e79cbfeb
omgitskuei <kueifengtungchris@gmail.com>
2022-11-19 19:39:28 +0800

    Update cyberdeck_stats_monitor.py to comply with PEP 8

--- RPi400/Cyberdeck_Stats_Monitor/python/cyberdeck_stats_monitor.py
+++ RPi400/Cyberdeck_Stats_Monitor/python/cyberdeck_stats_monitor.py
@@ -9,67 +9,50 @@ import decimal
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 
-# init logger
 logging.basicConfig(level=logging.DEBUG)
 
-# import necessary waveshare_epd library
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'library')
 logging.info("Retrieving python library for driving Waveshare 2.13in ePaper HAT...")
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+                      'library')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 from waveshare_epd import epd2in13_V3
+
 logging.info("Done.")
 
-# save path to .../RPiDev/RPi400/Cyberdeck_Stats_Monitor/pic
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+logging.info("Get path for /pic...")
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+                      'pic')
+logging.info("Done.")
 
-# init global vars
-cpuTemp = ""
-cpuUsage = ""
-ramTot = ""
-ramUsed = ""
-ramFree = ""
-diskUsage = ""
-time = ""
-ip = ""
-epd = None
-# ePaper display dimension is 250x122
-displayWidth = 250
-displayHeight = 122
 
 # Note: Returns str, add "°C" to the result
-def getCPUTemp_os():
+def get_gpu_temp_via_os():
     res = os.popen('vcgencmd measure_temp').readline()
-    return(res.replace("temp=","").replace("'C\n",""))
+    return res.replace("temp=", "").replace("'C\n", "")
+
 
 # Note: Returns float, add "%" to the result
-def get_CPUUsage():
+def get_cpu_usage():
     return str(psutil.cpu_percent())
 
+
 # Note: Returns RAM info (units in bytes) as a dict
-def getRAMinfo():
-    ramInfo_raw = psutil.virtual_memory()
-    return {
-        # "total" : ramInfo_raw[0],
-        # "available" : ramInfo_raw[1],
-        "percent" : ramInfo_raw[2],
-        # "used" : ramInfo_raw[3],
-        # "free" : ramInfo_raw[4],
-        # "active" : ramInfo_raw[5],
-        # "inactive" : ramInfo_raw[6],
-        # "buffers" : ramInfo_raw[7],
-        # "cached" : ramInfo_raw[8],
-        # "shared" : ramInfo_raw[9],
-        # "slab" : ramInfo_raw[10],
-        }
+def get_ram_info():
+    ram_info_raw = psutil.virtual_memory()
+    return {"percent": ram_info_raw[2]}
+
 
 # Note: Returns disk usage as a float
-def get_diskUsage():
-    return str(roundDownToOneDecimal(gpiozero.DiskUsage().usage))
+def get_disk_usage():
+    return str(round_down_to_one_decimal(gpiozero.DiskUsage().usage))
 
-# Note: Takes a float and truncates it to one decimal places, returns the number as a decimal
-def roundDownToOneDecimal(aFloat):
-    return decimal.Decimal(aFloat).quantize(decimal.Decimal('.1'), rounding=decimal.ROUND_DOWN)
+
+# Note: Takes a float and truncates it to one decimal places, returns the number
+# as a decimal
+def round_down_to_one_decimal(a_float):
+    return decimal.Decimal(a_float).quantize(decimal.Decimal('.1'),
+                                             rounding=decimal.ROUND_DOWN)
 
 
 # main...
@@ -81,84 +64,99 @@ def main():
         epd.init()
         # epd.Clear(0xFF)
         logging.info("Done.")
-        
+
         # get data
         logging.info("Getting CPU temperature...")
-        cpuTemp = getCPUTemp_os()  + "°C"
-        logging.info(cpuTemp)
+        cpu_temp = '{}°C'.format(get_gpu_temp_via_os())
+        logging.info(cpu_temp)
         logging.info("Done.")
-        
+
         logging.info("Getting CPU usage...")
-        cpuUsage = '{}%'.format(get_CPUUsage()) 
-        logging.info(cpuUsage)
+        cpu_usage = '{}%'.format(get_cpu_usage())
+        logging.info(cpu_usage)
         logging.info("Done.")
-        
+
         logging.info("Getting RAM usage...")
-        ramInfo = '{}%'.format(getRAMinfo()['percent'])
-        logging.info(ramInfo)
+        ram_info = '{}%'.format(get_ram_info()['percent'])
+        logging.info(ram_info)
         logging.info("Done.")
-        
+
         logging.info("Getting disk usage...")
-        diskUsage = '{}%'.format(get_diskUsage())
-        logging.info(diskUsage)
+        disk_usage = '{}%'.format(get_disk_usage())
+        logging.info(disk_usage)
         logging.info("Done.")
-        
+
         logging.info("Getting timestamp...")
         time = str(datetime.now())[0:str(datetime.now()).index('.')]
         logging.info(time)
         logging.info("Done.")
-        
+
         logging.info("Getting IP address...")
-        ip = os.popen('hostname -I').readline()[0:-2] # NOTE: ends with a SPACE and an ENTER, need to remove last 2 chars
-        logging.info(ip) 
+        # NOTE: ends with a SPACE and an ENTER, need to remove last 2 chars
+        ip = os.popen('hostname -I').readline()[0:-2]
+        logging.info(ip)
         logging.info("Done.")
-        
+
         # init fonts
         logging.info("Getting fonts...")
         font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
         font13 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 14)
         logging.info("Done.")
-        
+
         # draw display
         logging.info("Writing bmp image into buffer...")
         image = Image.open(os.path.join(picdir + '/bmps', 'combined.bmp'))
         draw = ImageDraw.Draw(image)
         logging.info("Done.")
-        
+
         logging.info("Writing data into buffer...")
-        topPadding = 15
-        leftPadding = 50
+        top_padding = 15
+        left_padding = 50
         # top row - CPU usage %, CPU temperature
-        draw.text((leftPadding,           0 + topPadding), cpuUsage, font = font24, fill = 0)
-        draw.text((120 + leftPadding, 0 + topPadding), cpuTemp, font = font24, fill = 0)
+        draw.text((left_padding, 0 + top_padding),
+                  cpu_usage,
+                  font=font24,
+                  fill=0)
+        draw.text((120 + left_padding, 0 + top_padding),
+                  cpu_temp,
+                  font=font24,
+                  fill=0)
         # middle row - RAM usage %, Disk used %
-        draw.text((leftPadding,           45 + topPadding), ramInfo, font = font24, fill = 0)
-        draw.text((120 + leftPadding, 45 + topPadding), diskUsage, font = font24, fill = 0)
+        draw.text((left_padding, 45 + top_padding),
+                  ram_info,
+                  font=font24,
+                  fill=0)
+        draw.text((120 + left_padding, 45 + top_padding),
+                  disk_usage,
+                  font=font24,
+                  fill=0)
         # bottom row - Timestamp, IP address
-        draw.text((5,                             85 + topPadding), time[0:-3] + ((" | " + ip) if ((ip == None) or (len(ip) > 0)) else ""), font = font13, fill = 0) # remove seconds, if no ip then ""
+        draw.text((5, 85 + top_padding),
+                  time[0:-3] + ((" | " + ip) if (ip is None or len(ip) > 0) else ""),
+                  font=font13,
+                  fill=0)  # remove seconds, if no ip then ""
         logging.info("Done.")
-        
+
         logging.info("Displaying the buffer onto the ePaper")
         epd.display(epd.getbuffer(image))
         logging.info("Done.")
-        
+
         # sleep
         logging.info("Putting ePaper display to sleep...")
         epd.sleep()
         logging.info("Done.")
-        
+
         exit()
-        
+
     except IOError as e:
         logging.error(e)
-        
-    except KeyboardInterrupt:    
-        logging.info("User Pressed [Ctrl] + [c] - KeyboardInterrupt ending script...")
+
+    except KeyboardInterrupt:
+        logging.info("User Pressed [Ctrl] + [c]. KeyboardInterrupt ending script...")
         epd2in13_V3.epdconfig.module_exit()
-        
         logging.info("Done.")
         exit()
 
+
 if __name__ == '__main__':
     main()
-    
